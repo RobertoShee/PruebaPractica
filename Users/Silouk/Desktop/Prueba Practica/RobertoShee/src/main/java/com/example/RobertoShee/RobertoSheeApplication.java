85bd8de17184aed3b2dd8b17879c74e1937a5ec4
RobertoShee <robert.shee94@gmail.com>
2024-11-16 13:03:13 -0300

    Primer commit

---
+++ Users/Silouk/Desktop/Prueba Practica/RobertoShee/src/main/java/com/example/RobertoShee/RobertoSheeApplication.java
@@ -0,0 +1,13 @@
+package com.example.RobertoShee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobertoSheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobertoSheeApplication.class, args);
+	}
+
+}
---
+++ Users/Silouk/Desktop/Prueba Practica/RobertoShee/src/test/java/com/example/RobertoShee/RobertoSheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RobertoShee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobertoSheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
